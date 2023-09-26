40fe9bda7dcbae0f64917753297318290cbdb3bb
NEMMERTDEVELOPER <139520295+NEMMERTDEVELOPER@users.noreply.github.com>
2023-09-25 22:34:43 -0400

    1st commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// document.getElementById("count").innerText = 5
+
+ //let count = 5
+//count + 1
+ //count = 0
+//count = count + 1
+
+ //console.log(count)
+
+ //let bonusPoints = 50
+ //bonusPoints = bonusPoints + 50
+  //console.log(bonusPoints)
+  //bonusPoints = bonusPoints - 45
+  //console.log(bonusPoints)
+  //bonusPoints = bonusPoints + 70
+  //console.log(bonusPoints)
+
+
+// 1. Create a variable, my Age, and set its value to your age
+//let myAge = 41
+
+//2. log the myAge variable to the console
+//console.log(myAge)
+
+//let firstBatch = 5
+//let secondBatch = 7
+
+//let count = firstBatch + secondBatchconsole.log(count)
+
+
+// 1. Create two variables, myAge, and humanDogRatio
+// 2. Multiply the two tegethr and store the result in myDogAge
+// 3. log myDogAge to the console
+
+//let myAge = 41
+//let humanDogRatio = 7
+//let myDogAge = myAge * humanDogRatio
+//console.log(myDogAge)
+
+function increment() {
+    console.log("button was clicked")
+}
